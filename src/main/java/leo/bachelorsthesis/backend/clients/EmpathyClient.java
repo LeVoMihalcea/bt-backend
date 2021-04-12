0d393c0aca3e8db15ce515fe1f9e5e192ca4e0d8
Leo Mihalcea <leomi@LEOS-LAPTOP>
2021-04-12 19:10:08 +0300

    added empathy prod url

--- src/main/java/leo/bachelorsthesis/backend/clients/EmpathyClient.java
+++ src/main/java/leo/bachelorsthesis/backend/clients/EmpathyClient.java
@@ -1,10 +1,8 @@
 package leo.bachelorsthesis.backend.clients;
 
 import feign.Headers;
-import feign.Param;
 import feign.RequestLine;
 import leo.bachelorsthesis.backend.domain.dtos.empathy.EmpathyResponseDTO;
-import org.springframework.messaging.handler.annotation.Header;
 
 public interface EmpathyClient {
     @RequestLine("POST /")
