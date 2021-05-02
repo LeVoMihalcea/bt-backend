0b5f1ff13f858327e25aee4b8d70ef949afe4243
Leo Mihalcea <leomi@LEOS-LAPTOP>
2021-05-03 00:52:35 +0300

    now mapping the dominant trait

--- src/main/java/leo/bachelorsthesis/backend/builders/Mapper.java
+++ src/main/java/leo/bachelorsthesis/backend/builders/Mapper.java
@@ -6,22 +6,37 @@ import leo.bachelorsthesis.backend.constants.ImageConstants;
 import leo.bachelorsthesis.backend.domain.dtos.empathy.EmpathyResponseDTO;
 import leo.bachelorsthesis.backend.error.exceptions.ApiJsonError;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Mapper {
-    public static String mapEmpathyResponseToJson(EmpathyResponseDTO empathyResponse){
+    public static String mapEmpathyResponseToJson(EmpathyResponseDTO empathyResponse) {
         Map<String, String> map = new HashMap<>();
         ObjectMapper objectMapper = new ObjectMapper();
 
         map.put("confusion", String.valueOf(empathyResponse.getConfusion()));
         map.put("happiness", String.valueOf(empathyResponse.getHappiness()));
         map.put("sadness", String.valueOf(empathyResponse.getSadness()));
+        map.put("message", getDominantTrait(empathyResponse));
+        map.put("maxValue", String.valueOf(Collections.max(List
+                .of(empathyResponse.getConfusion(), empathyResponse.getHappiness(), empathyResponse.getSadness()))));
 
         try {
             return objectMapper.writeValueAsString(map);
         } catch (JsonProcessingException e) {
             throw new ApiJsonError(ImageConstants.INVALID_RESPONSE);
         }
+    }
+
+    private static String getDominantTrait(EmpathyResponseDTO empathyResponse) {
+        Integer max = Collections.max(
+                List.of(empathyResponse.getConfusion(), empathyResponse.getHappiness(), empathyResponse.getSadness()));
+        if(empathyResponse.getConfusion() == max)
+            return ImageConstants.CONFUSION_IS_DOMINANT;
+        if(empathyResponse.getHappiness() == max)
+            return ImageConstants.HAPPINESS_IS_DOMINANT;
+        return ImageConstants.SADNESS_IS_DOMINANT;
     }
 }
--- src/main/java/leo/bachelorsthesis/backend/constants/ImageConstants.java
+++ src/main/java/leo/bachelorsthesis/backend/constants/ImageConstants.java
@@ -3,4 +3,7 @@ package leo.bachelorsthesis.backend.constants;
 public class ImageConstants {
     public static final String INVALID_RESPONSE = "The response from Empathy is invalid";
 
+    public static final String CONFUSION_IS_DOMINANT = "Confusion detected";
+    public static final String HAPPINESS_IS_DOMINANT = "Happiness detected";
+    public static final String SADNESS_IS_DOMINANT = "Sadness detected";
 }
